9325ef900953f34167f9d6e87e2654130392385d
Andy R <ProgrammerAndy@gmail.com>
2019-06-01 15:31:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def get_authentication(p_cursor,p_username, p_password):
        sys.exit("Unable to query database...investigate\n" + traceback.format_exc())
 
     if len(v_auth_info) > 0:
-       return v_auth_info[0]
+       return v_auth_info[0][0]
     else:
        return -1
        
