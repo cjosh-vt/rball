494acd1996e75853b3ac40bea582c6cf18c0e618
Andy R <ProgrammerAndy@gmail.com>
2019-06-02 13:54:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import traceback
 import sys
 import psycopg2
 
+
+# Define all application endpoints beginning Here
 app = Flask(__name__)
 
 @app.route("/login",methods=['GET'])
@@ -43,24 +45,36 @@ def playerInfo():
     else:
        return v_player_info
 
+#This is the end of all application endpoints.  Below here are the functions related to the database
+#which are USED by the application endpoints.
+
 def connect_to_postgres():
     return common_functions_rball.get_pg_connection('rball_app','vMBY8kU3E67Cz2ZC','127.0.0.1','nw_rball_app')
 
-def get_player_info(p_cursor,p_player_id):
+def execute_a_query(p_connection, p_query):
+    v_cursor = p_connection.cursor()
+    
+    try:
+       v_cursor.execute(p_query)
+       v_query_result=v_cursor.fetchall()
+    except psycopg2.errors.SyntaxError:
+       p_connection.rollback()
+    except psycopg2.errors.InsufficientPrivilege:
+       p_connection.rollback()
+    except psycopg2.errors.UndefinedTable:
+       p_connection.rollback()
+    except:
+       print ("Failed to execute query:  \n" + p_query + "\n" + traceback.format_exc())
+    else:
+       return v_query_result
+
+def get_player_info(p_connection,p_player_id):
     v_query = ("select player_first_name, player_last_name, player_phone, player_email, season_description, skill_description, is_administrator from rball_app.player_info where player_id = " + str(p_player_id))
     try:
-       p_cursor.execute(v_query)
-       v_player_info = p_cursor.fetchall()
-    except psycopg2.errors.SyntaxError:
-       v_conn.rollback()
-    except psycopg2.errors.InsufficientPrivilege:
-       v_conn.rollback()
-    except psycopg2.errors.UndefinedTable:
-       v_conn.rollback()
+       v_player_info = execute_a_query(v_query)
     except:
        print ("Unable to query player information...investigate\n" + traceback.format_exc())
     else:
-       print ("v_player_info = " + str(v_player_info))
        if v_player_info is not None:
           return jsonify(player_first_name=v_player_info[0][0],
              player_last_name=v_player_info[0][1],
@@ -72,17 +86,10 @@ def get_player_info(p_cursor,p_player_id):
        else:
           return -1
 
-def get_authentication(p_cursor,p_username, p_password):
+def get_authentication(p_connection,p_username, p_password):
     v_query = ("select player_id from rball_app.auth_login where username = '" + p_username + "' and password = '" + p_password + "'")
     try:
-       p_cursor.execute(v_query)
-       v_auth_info = p_cursor.fetchall()
-    except psycopg2.errors.SyntaxError:
-       v_conn.rollback()
-    except psycopg2.errors.InsufficientPrivilege:
-       v_conn.rollback()
-    except psycopg2.errors.UndefinedTable:
-       v_conn.rollback()
+       v_auth_info = execute_a_query(v_query)
     except:
        print("Unable to query database...investigate\n" + traceback.format_exc())
  
@@ -93,17 +100,17 @@ def get_authentication(p_cursor,p_username, p_password):
 
 def get_connection():
     try:
-       global g_cursor
-       global v_conn
        v_conn=connect_to_postgres()
        v_conn.autocommit=False
-       g_cursor=v_conn.cursor()
     except:
        print ("Whoops...you're still dumb...\n" + traceback.format_exc())
        
 
+#Main is invoked when the application is started on the server.
 if __name__ == '__main__':
 
+    #We start by connecting to the postgres database
     get_connection()
     
+    #Begin running the application listener
     app.run('0.0.0.0',port=80)
